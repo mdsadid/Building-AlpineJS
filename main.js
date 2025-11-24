ccfb9d01c63dce9e260b253c873e16293d2407ec
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2025-11-24 12:32:17 +0600

    Making Things Reactive

---
+++ main.js
@@ -0,0 +1,45 @@
+let root = document.querySelector('[x-data]');
+let rawData = getInitialData();
+let data = observe(rawData);
+
+function getInitialData() {
+	let dataString = root.getAttribute('x-data');
+
+	return eval(`(${dataString})`);
+}
+
+function observe(data) {
+	return new Proxy(data, {
+		set(target, prop, value) {
+			target[prop] = value;
+
+			refreshDOM(root);
+
+			return true;
+		}
+	});
+}
+
+refreshDOM(root);
+
+function refreshDOM(root) {
+	walkDOM(root, el => {
+		if (el.hasAttribute('x-text')) {
+			let expression = el.getAttribute('x-text');
+
+			el.innerText = eval(`(data.${expression})`);
+		}
+	});
+}
+
+function walkDOM(el, callback) {
+	callback(el);
+
+	el = el.firstElementChild;
+
+	while (el) {
+		walkDOM(el, callback);
+
+		el = el.nextElementSibling;
+	}
+}
